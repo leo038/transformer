e8b2e1e117049f2adfe7c22552a4580fd1fdd123
liyong <819246955@qq,com>
2024-11-05 17:14:25 +0800

    add decoder

--- main.py
+++ main.py
@@ -42,15 +42,15 @@ class MutiHeadAttention(nn.Module):
 
     def __init__(self, d_model=512, d_k=64, d_v=128):
         super().__init__()
-        self.num_head = d_model//d_v
+        self.num_head = d_model // d_v
         self.d_k = d_k
         self.d_v = d_v
         if d_k == d_v:
             self.project_qkv = nn.Linear(in_features=d_model, out_features=3 * d_model, bias=False)
         else:
             ##if d_k!= d_v, we need to calculate q,k,v separately
-            self.project_q = nn.Linear(in_features=d_model, out_features=self.d_k*self.num_head, bias=False)
-            self.project_k = nn.Linear(in_features=d_model, out_features=self.d_k*self.num_head, bias=False)
+            self.project_q = nn.Linear(in_features=d_model, out_features=self.d_k * self.num_head, bias=False)
+            self.project_k = nn.Linear(in_features=d_model, out_features=self.d_k * self.num_head, bias=False)
             self.project_v = nn.Linear(in_features=d_model, out_features=d_model, bias=False)
 
         self.softmax = nn.Softmax()
@@ -74,6 +74,45 @@ class MutiHeadAttention(nn.Module):
                 Q = q[:, :, n_h * self.d_k: (n_h + 1) * self.d_k]
                 K = k[:, :, n_h * self.d_k: (n_h + 1) * self.d_k]
                 V = v[:, :, n_h * self.d_v: (n_h + 1) * self.d_v]
+            attention_score = torch.bmm(Q, K.transpose(1, 2))
+            attention_score = self.softmax(attention_score / np.sqrt(self.d_k))
+
+            out = torch.bmm(attention_score, V)
+            if attention_out is None:
+                attention_out = out
+            else:
+                attention_out = torch.cat((attention_out, out), 2)  # 把多头结果进行concat
+        out = self.project_out(attention_out)
+
+        return out
+
+
+class CrossMutiHeadAttention(nn.Module):
+
+    def __init__(self, d_model=512, d_k=64, d_v=128):
+        super().__init__()
+        self.num_head = d_model // d_v
+        self.d_k = d_k
+        self.d_v = d_v
+
+        self.project_q = nn.Linear(in_features=d_model, out_features=self.d_k * self.num_head, bias=False)
+        self.project_k = nn.Linear(in_features=d_model, out_features=self.d_k * self.num_head, bias=False)
+        self.project_v = nn.Linear(in_features=d_model, out_features=d_model, bias=False)
+
+        self.softmax = nn.Softmax()
+        self.project_out = nn.Linear(in_features=d_model, out_features=d_model, bias=False)
+
+    def forward(self, encoder_output=None, pre_output=None):
+
+        attention_out = None
+
+        q = self.project_q(encoder_output)
+        k = self.project_k(encoder_output)
+        v = self.project_v(pre_output)
+        for n_h in range(self.num_head):
+            Q = q[:, :, n_h * self.d_k: (n_h + 1) * self.d_k]
+            K = k[:, :, n_h * self.d_k: (n_h + 1) * self.d_k]
+            V = v[:, :, n_h * self.d_v: (n_h + 1) * self.d_v]
             attention_score = torch.bmm(Q, K.transpose(1, 2))
             attention_score = self.softmax(attention_score / np.sqrt(self.d_k))
 
@@ -120,6 +159,29 @@ class EncoderLayer(nn.Module):
         return out
 
 
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model=512):
+        super().__init__()
+        self.mha = MutiHeadAttention()
+        self.cross_mha = CrossMutiHeadAttention()
+        self.ffn = FeedForward()
+        self.layer_nrom = nn.LayerNorm(normalized_shape=d_model)
+
+    def forward(self, encoder_output, pre_output):
+        out = self.mha(pre_output)
+        out = self.layer_nrom(out)
+        out_mha = pre_output + out
+
+        out = self.cross_mha(encoder_output=encoder_output, pre_output=out_mha)
+        out = self.layer_nrom(out)
+        out_cross_mha = out_mha + out
+
+        out = self.ffn(out_cross_mha)
+        out = self.layer_nrom(out)
+        out = out_cross_mha + out
+        return out
+
+
 class Encoder(nn.Module):
     def __init__(self, encoder_layer_num=6):
         super().__init__()
@@ -131,15 +193,34 @@ class Encoder(nn.Module):
         return x
 
 
+class Decoder(nn.Module):
+    def __init__(self, decoder_layer_num=6):
+        super().__init__()
+        self.decoder = nn.ModuleList([DecoderLayer() for _ in range(decoder_layer_num)])
+
+    def forward(self, encoder_output=None, pre_output=None):
+        for layer in self.decoder:
+            x = layer(encoder_output=encoder_output, pre_output=pre_output)
+            pre_output = x
+        return x
+
+
 class Transformer(nn.Module):
-    def __init__(self):
+    def __init__(self, d_model=512):
         super().__init__()
         self.pre_process = PreProcess()
         self.encoder = Encoder()
+        self.decoder = Decoder()
+        self.out_project = nn.Linear(in_features=d_model, out_features=vocabulary_size)
+        self.softmax = nn.Softmax()
 
-    def forward(self, input_seq):
+    def forward(self, input_seq=None, pre_output=None):
         out = self.pre_process(input_seq).float()
-        out = self.encoder(out)
+        pre_output = self.pre_process(pre_output).float()
+        encoder_output = self.encoder(out)
+        out = self.decoder(encoder_output=encoder_output, pre_output=pre_output)
+        out = self.out_project(out)
+        out = self.softmax(out)
         return out
 
 
@@ -149,9 +230,14 @@ if __name__ == "__main__":
         [3, 1, 3, 4, 9, 99]
     ])
 
+    pre_output = torch.tensor([
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1]
+    ])
+
     transformer = Transformer()
     print("########################网络结构########################")
     print(transformer)
     print("######################################################")
-    res = transformer(input_seq)
+    res = transformer(input_seq, pre_output)
     print(f"输出大小：{res.shape}")
